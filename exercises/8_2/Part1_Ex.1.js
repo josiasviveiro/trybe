445c718beeeb3974caef1f4d866c345c341d9b43
Josias Viveiro <josiasviveiro@gmail.com>
2020-05-04 19:29:36 -0300

    Exs - part1 ex.1

---
+++ exercises/8_2/Part1_Ex.1.js
@@ -0,0 +1,12 @@
+// 1 - Acessando propriedades de um objeto por variável
+var testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+};
+ 
+var playerNumber = 16;
+var player = testObj[playerNumber];
+
+console.log(player);
+  
