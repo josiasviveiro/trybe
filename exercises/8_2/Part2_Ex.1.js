9e0d22adfc98dde4678ddadf5765bbc6d3b09724
Josias Viveiro <josiasviveiro@gmail.com>
2020-05-04 20:32:01 -0300

    Part2 Ex.1

---
+++ exercises/8_2/Part2_Ex.1.js
@@ -0,0 +1,30 @@
+// 1 - Crie uma função para adicionar o turno da manhã na lesson2. 
+// Essa função deve possuir três parâmetros, sendo eles: 
+// o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+  
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+  
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+function newKeyValue(object, newkey, newValue) {
+  object[newkey] = newValue;
+  return console.log(object);
+}
+
+newKeyValue (lesson2, 'turno', 'manhã');
