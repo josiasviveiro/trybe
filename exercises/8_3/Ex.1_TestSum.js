c763e38929e993f0471d520eb75f5647f6df5e51
Josias Viveiro <josiasviveiro@gmail.com>
2020-05-05 19:52:57 -0300

    Ex.1

---
+++ exercises/8_3/Ex.1_TestSum.js
@@ -0,0 +1,27 @@
+/*
+ 1. A função sum(a, b) retorna a soma do parâmetro a com o b
+*/
+
+const assert = require('assert');
+
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+
+  return a + b;
+}
+
+// implemente seus testes aqui
+
+// 1. Teste se o retorno de sum(4, 5) é 9
+assert.equal(sum(4, 5), 9, 'Resultado esperado: 9');
+
+// 2. Teste se o retorno de sum(0, 0) é 0
+assert.equal(sum(0, 0), 0, "Resultado esperado: 0")
+
+// 3. Teste se a função sum lança um erro quando os parametros são 4 e "5"(string 5)
+assert.throws( () => {sum(4, '5')}, 'A função não lança mensagem de erro');
+
+// 4. Teste se a mensagem de erro é “parameters must be numbers” quando realizar a chamada sum(4, "5")
+assert.throws(() => {sum(4, '5')},/^Error: parameters must be numbers$/);
